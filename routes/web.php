9f2e59fb9a9de7bd3cefe58bc3b99d3fb1137cf9
afizihusaini93 <afizihusaini93@gmail.com>
2025-04-11 22:32:28 +0800

    Update routes for login

--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,10 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CompanyController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
+Route::get('/', [AuthController::class, 'showLogin']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-
     Route::resource('companies', CompanyController::class);
 });
