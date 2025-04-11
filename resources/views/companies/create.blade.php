3e78faea895959ea36453bbfc2b1f500155731f5
afizihusaini93 <afizihusaini93@gmail.com>
2025-04-11 22:47:31 +0800

    Update view of companies index, create and edit

--- resources/views/companies/create.blade.php
+++ resources/views/companies/create.blade.php
@@ -3,14 +3,20 @@
 @section('title', 'Create Company')
 
 @section('content')
-<h2>Create Company</h2>
+<div class="card shadow">
+    <div class="card-header bg-primary text-white">
+        <h5 class="mb-0">Create Company</h5>
+    </div>
+    <div class="card-body">
+        <form method="POST" action="{{ route('companies.store') }}" enctype="multipart/form-data">
+            @csrf
 
-<form method="POST" action="{{ route('companies.store') }}" enctype="multipart/form-data">
-    @csrf
+            @include('companies.partials.form')
 
-    @include('companies.partials.form')
+            <button type="submit" class="btn btn-success">Save</button>
+            <a href="{{ route('companies.index') }}" class="btn btn-secondary">Cancel</a>
+        </form>
+    </div>
+</div>
 
-    <button type="submit" class="btn btn-success">Save</button>
-    <a href="{{ route('companies.index') }}" class="btn btn-secondary">Cancel</a>
-</form>
 @endsection
--- resources/views/companies/edit.blade.php
+++ resources/views/companies/edit.blade.php
@@ -3,15 +3,21 @@
 @section('title', 'Edit Company')
 
 @section('content')
-<h2>Edit Company</h2>
+<div class="card shadow">
+    <div class="card-header bg-primary text-white">
+        <h5 class="mb-0">Edit Company</h5>
+    </div>
+    <div class="card-body">
+        <form method="POST" action="{{ route('companies.update', $company->id) }}" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
 
-<form method="POST" action="{{ route('companies.update', $company->id) }}" enctype="multipart/form-data">
-    @csrf
-    @method('PUT')
+            @include('companies.partials.form')
 
-    @include('companies.partials.form')
+            <button type="submit" class="btn btn-primary">Update</button>
+            <a href="{{ route('companies.index') }}" class="btn btn-secondary">Cancel</a>
+        </form>
+    </div>
+</div>
 
-    <button type="submit" class="btn btn-primary">Update</button>
-    <a href="{{ route('companies.index') }}" class="btn btn-secondary">Cancel</a>
-</form>
 @endsection
